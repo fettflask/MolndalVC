8bfe211bd6a0366fadfad441c99689477ee8c60e
Albin Edwall Laptop <edwall.albinn@gmail.com>
2024-11-21 10:41:10 +0100

    initial push

--- Sidor/minaSidor.php
+++ Sidor/minaSidor.php
@@ -8,6 +8,68 @@
         die();
     }
 
-    echo $_SESSION["namn"];
-    echo $_SESSION["pnr"];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="../IMG/favicon.png">
+    <link rel="stylesheet" href="../Stylesheets/headerStyle.css">
+    <link rel="stylesheet" href="../Stylesheets/minaSidorStyle.css">
+    <title>Mina Sidor</title>
+</head>
+<body>
+    <header>
+        <div id="companylogo">
+            <a href="index.php">
+                <img src="../IMG/MölndalLogo.png">
+            </a>
+        </div>
+
+        <div id="topnav">
+            <div class="navbox">
+                <a href="">Nyheter</a>
+            </div>
+
+            <div class="navbox">
+                <a href="">Sjukdomar & Besvär</a>
+            </div>
+
+            <div class="navbox">
+                <a href="">Hälsoråd & Tips</a>
+            </div>
+
+            <div class="navbox">
+                <a href="">Mer</a>
+            </div>
+        </div>
+
+        <div id="namn">
+            <?php
+                echo "Välkommen, " . $_SESSION["namn"];
+            ?>
+        </div>
+    </header>
+
+    <main>
+        <h2>Hur kan vi hjälpa dig?</h2>
+        <div id="mastergrid">
+            <div class="gridelement">
+                hej
+            </div>
+            <div class="gridelement">
+                HALLÅ
+            </div>
+            <div class="gridelement">
+                Tjena
+            </div>
+            <div class="gridelement">
+                Morsning
+            </div>
+        </div>
+    </main>
+
+</body>
+</html>
