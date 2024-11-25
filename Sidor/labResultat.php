228ec53e4180da07cc5ad83bb0e9fb25899d39c7
Jak <jack.kartler112@gmail.com>
2024-11-25 08:51:58 +0100

    Försökt ändra icon på labResultat sidan

--- Sidor/labResultat.php
+++ Sidor/labResultat.php
@@ -62,7 +62,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../Stylesheets/headerStyle.css">
-    <title>Registrera</title>
+    <title>Provsvar</title>
 </head>
 <body>
     <header>
