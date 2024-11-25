60b5b94e600e6ce943f064b91281244f70a035b7
Jak <jack.kartler112@gmail.com>
2024-11-25 16:31:21 +0100

    Fixade filtrering för vårdmöten beroende på vad som finns inmatat i ERP Next

--- Sidor/patientJournal.php
+++ Sidor/patientJournal.php
@@ -142,11 +142,45 @@
                     echo '<p>Avdelning: ' . htmlspecialchars($journal['medical_department'] ?? 'N/A') . '</p>';
                     
                     echo '<p>Journal antäckning:<br>';
-                    echo '' . htmlspecialchars($journal['encounter_comment'] ?? 'N/A') . '</p>';
+                    echo '' . htmlspecialchars($journal['encounter_comment'] ?? 'Finns ingen antäckning för mötet') . '</p>';
                     if (isset($journal['name'])) {
                         $JournalDetails = getPatientEncountersDetails($journal);
                         $JournalDetails = json_decode($JournalDetails, true);
 
+                        $hasDiagnosis = false;
+                        $hasProcedure = false;
+                        $hasSymptoms = false;
+                        $hasMedicine = false;
+
+                        foreach ($JournalDetails['data']['diagnosis'] as $diagnosis) {
+                            if (!empty($JournalDetails['data']['diagnosis'])) {
+                                $hasDiagnosis = true;
+                                break;
+                            }
+                        }
+
+                        foreach ($JournalDetails['data']['procedure_prescription'] as $procedure) {
+                            if (!empty($JournalDetails['data']['procedure_prescription'])) {
+                                $hasProcedure = true;
+                                break;
+                            }
+                        }
+
+                        foreach ($JournalDetails['data']['symptoms'] as $symptoms) {
+                            if (!empty($JournalDetails['data']['symptoms'])) {
+                                $hasSymptoms = true;
+                                break;
+                            }
+                        }
+
+                        foreach ($JournalDetails['data']['drug_prescription'] as $symptoms) {
+                            if (!empty($JournalDetails['data']['drug_prescription'])) {
+                                $hasMedicine = true;
+                                break;
+                            }
+                        }
+
+                        if($hasDiagnosis){
                             if (isset($JournalDetails['data']['diagnosis']) && is_array($JournalDetails['data']['diagnosis'])) {
                                 foreach ($JournalDetails['data']['diagnosis'] as $Detail) {
                                     echo '<p>';
@@ -154,7 +188,9 @@
                                     echo '</p>';                                    
                                 }
                             }
-                    
+                        }
+                        
+                        if($hasSymptoms){
                             echo '<details>';
                             echo '<summary>Symptom vid möte</summary>';
                             if (isset($JournalDetails['data']['symptoms']) && is_array($JournalDetails['data']['symptoms'])) {
@@ -165,7 +201,9 @@
                                 echo '</ul></div>';
                             }
                             echo '</details>';
+                        }
 
+                        if($hasProcedure){
                             echo '<details>';
                             echo '<summary>Genomförd procedur</summary>';
                             if (isset($JournalDetails['data']['procedure_prescription']) && is_array($JournalDetails['data']['procedure_prescription'])) {
@@ -178,29 +216,32 @@
                                         
                                     }
                                 }
-                            }
-                            echo '</details>';
-
-                            echo '<details>';
-                            echo '<summary>Utskriven medicin</summary>';
-                            if (isset($JournalDetails['data']['drug_prescription']) && is_array($JournalDetails['data']['drug_prescription'])) {
-                                foreach ($JournalDetails['data']['drug_prescription'] as $Detail) {
-                                    echo '<div>';
-                                        echo '<p>';
-                                        echo 'Medicin: ' . htmlspecialchars($Detail['medication'] ?? 'N/A') . '<br>';
-                                        echo 'Dos: ' . htmlspecialchars($Detail['dosage_form'] ?? 'N/A') . '<br>';
-                                        echo 'Dos intervall: ' . htmlspecialchars($Detail['dosage'] ?? 'N/A') . '<br>';
-                                        echo 'Ska tas i ' . htmlspecialchars($Detail['interval_uom'] ?? 'N/A') . '<br>';
-                                        echo '</p>';
-                                    echo '</div>'; 
-                                }
-                            }
-                            echo '</details>';
+                            
                             echo '</details>';
                         }
-                        
                     }
-                echo '</div>';
+                    
+                    if($hasMedicine){
+                        echo '<details>';
+                        echo '<summary>Utskriven medicin</summary>';
+                        if (isset($JournalDetails['data']['drug_prescription']) && is_array($JournalDetails['data']['drug_prescription'])) {
+                            foreach ($JournalDetails['data']['drug_prescription'] as $Detail) {
+                                echo '<div>';
+                                    echo '<p>';
+                                    echo 'Medicin: ' . htmlspecialchars($Detail['medication'] ?? 'N/A') . '<br>';
+                                    echo 'Dos: ' . htmlspecialchars($Detail['dosage_form'] ?? 'N/A') . '<br>';
+                                    echo 'Dos intervall: ' . htmlspecialchars($Detail['dosage'] ?? 'N/A') . '<br>';
+                                    echo 'Ska tas i ' . htmlspecialchars($Detail['interval_uom'] ?? 'N/A') . '<br>';
+                                    echo '</p>';
+                                echo '</div>'; 
+                            }
+                        }
+                        echo '</details>';
+                    }
+                    echo '</details>';
+                }   
+            }
+            echo '</div>';
 
             $patientVital = getPatientVitals();
             $patientVital = json_decode($patientVital, true);
@@ -210,13 +251,13 @@
                     echo '<h2>Vitalparametrar & mätningar</h2>';
                     foreach ($patientVital['data'] as $vital) {
                         echo '<details>';
-                            echo '<summary>Vitalvärden för ' . htmlspecialchars($vital['title'] ?? 'N/A') . '</summary>';                  
+                            echo '<summary>' . htmlspecialchars($vital['title'] ?? 'N/A') . '</summary>';                  
                             echo '<p>Kommentar: ' . htmlspecialchars($vital['vital_signs_note'] ?? 'N/A') . '</p>';
                             echo '<h4>Vitalparametrar</h4>';
                             echo '<p>';
                                 echo 'Puls: ' . htmlspecialchars($vital['pulse'] ?? 'N/A') . '<br>';
                                 echo 'Blodtryck: ' . htmlspecialchars($vital['bp'] ?? 'N/A') . '<br>';
-                                echo 'Andning: Andetag ' . htmlspecialchars($vital['respiratory_rate'] ?? 'N/A') . ' per minut.<br>';
+                                echo 'Andetag/minut: ' . htmlspecialchars($vital['respiratory_rate'] ?? 'N/A') . '<br>';
                                 echo 'Temperatur: ' . htmlspecialchars($vital['temperature'] ?? 'N/A') . 'C°';
                             echo '</p>';
 
