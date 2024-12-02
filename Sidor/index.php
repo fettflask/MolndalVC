2e028645ef9d0be5dce39cc839a1ec23f2fc8c9d
Albin Edwall <edwall.albinn@gmail.com>
2024-12-02 15:02:45 +0100

    index styling

--- Sidor/index.php
+++ Sidor/index.php
@@ -18,27 +18,16 @@
     <?php echoHead(); ?>
 
     <main>
-    <div class="infoText">
-        <h2>Mölndals Vårdcentral</h2>
-        <p>
-            Vårdcentralen är utrustad med moderna lokaler och tekniska lösningar som gör att personalen kan 
-            ge effektiv och säker vård. De ljusa, välkomnande rummen är designade för att skapa en lugn och 
-            trygg atmosfär för patienter. Här finns både en läkarstation, sjuksköterskemottagning och flera 
-            specialister som kan hjälpa till vid behov. Personalen består av engagerade läkare, sjuksköterskor, 
-            fysioterapeuter och andra yrkesgrupper som arbetar i nära samarbete för att ge patienterna den bästa 
-            möjliga vården.
-        </p>
-        <p>
-            En av vårdcentralens största styrkor är den personliga och tillgängliga vården. Patienterna kan oftast 
-            få snabbt hjälp vid mindre akuta besvär, men det erbjuds också långsiktig uppföljning vid kroniska 
-            sjukdomar eller rehabilitering efter skador. Det finns även möjlighet att boka tider för hälsokontroller, 
-            vaccinationer och andra förebyggande åtgärder för att säkerställa god hälsa på lång sikt.
-        </p>
-        <p>
-            Vårdcentralen i Mölndal är alltså inte bara en plats för akut vård, utan en central aktör i samhället 
-            som fokuserar på hela individens hälsa och välbefinnande.
-        </p>
-    </div>
+    <section id="split">
+        <div id="left">
+            <div id="leftP">
+                <h1>Din vårdcentral <br> i Mölndal</h1>
+                Mauris tempor sit amet neque quis fringilla. Curabitur luctus scelerisque velit, 
+                sed blandit nisi dapibus sed.
+            </div>
+        </div>
+        <div id="right"></div>
+    </section>
 
     <section class="Tjänster">
         <h3>Våra tjänster</h3>
