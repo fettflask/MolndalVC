d04817917866c383872340b967a47677443cb429
Jak <jack.kartler112@gmail.com>
2024-12-17 11:39:40 +0100

    databas error 5 jack e's fel

--- Sidor/patientJournal.php
+++ Sidor/patientJournal.php
@@ -6,10 +6,6 @@
         header("Location: patientLogin.php");
         die();
     }
-
-    $pdo = new PDO("mysql:dbname=grupp6;host=localhost", "sqllab", "Hare#2022");
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
 
 <!DOCTYPE html>
